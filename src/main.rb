c74a4824d5b3ada6e8405d683e54d0a858883366
nino reetok <reetok3@gmail.com>
2019-10-24 18:01:52 +0900

    made error

--- src/main.rb
+++ src/main.rb
@@ -3,15 +3,25 @@ begin
   require_relative 'lexer.rb'
   require_relative 'tokensteam.rb'
   require_relative 'parser.rb'
-
+  isdebug = false
+  if ARGV.include?("-d") || ARGV.include?("--debug")
+    isdebug = true
+  end
+  if isdebug
+    puts "ARGV Reading"
+  end
   inputs = ARGV
-  p inputs
-  p inputs[-1]
+  if isdebug
+    puts "inputs = #{ARGV}"
+  end
   if !inputs[-1]
     puts "File not specified."
     exit(1)
   end
   filename =  inputs[-1]
+  if isdebug
+    puts "filename = #{inputs[-1]}"
+  end
   if !File.exist?(filename)
     puts "File is not found"
     exit(1)
@@ -20,22 +30,50 @@ begin
     puts "Can't assign to keyword"
     exit(1)
   end
-  p filename
+  if isdebug
+    puts "---------------------------start lexer---------------------------"
+  end
   lexer = Lexer.new(filename)
+  if isdebug
+    puts "---------------------------finished lexer------------------------"
+  end
+  if isdebug
+    puts "---------------------------make lexer----------------------------"
+  end
   tokensteam = TokenSteam.new(lexer.run)
   token = []
+  if isdebug
+    puts "---------------------------made lexer----------------------------"
+  end
   tokensteam.getToken.size.times do |i|
     token.push(tokensteam.getToken[i-1][0])
   end
+  if isdebug
+    puts "---------------------------start parser--------------------------"
+  end
   parser = PinenutParser.new.parse(token.join)
-  tree = parser.run
-  p token
-  p tree
-rescue
-  puts error.message
-  puts $!
-  puts $@
-  puts "An error that occurred in the processing system"
-  puts "You can issue issues on Github."
-  puts "https://github.com/pinenut-programming-language/bittn/issues/new"
+  tree = parser.rune
+  if isdebug
+    puts "---------------------------finished parser-----------------------"
+    puts "tree = #{tree}"
+  end
+rescue => error
+  # puts error.message
+  # puts $!
+  if isdebug
+    puts "--------------------------!error!--------------------------------"
+    raise
+    puts "-----------------------------------------------------------------"
+  else
+    puts "--------------------------!error!--------------------------------"
+    puts error.message
+    puts "Traceback : "
+    puts $@
+    puts "-----------------------------------------------------------------"
+    puts "An error that occurred in the processing system"
+    puts "You can issue issues on Github."
+    puts "https://github.com/pinenut-programming-language/bittn/issues/new"
+    puts "-----------------------------------------------------------------"
+  end
+  exit(1)
 end
